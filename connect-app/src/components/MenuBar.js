5d22fd2a0a4e50e9343e0b6d3c3568ee4b791d7a
Silvin Pradhan <silvinpradhan95@gmail.com>
2020-10-18 11:16:39 -0500

    completed menubar

--- connect-app/src/components/MenuBar.js
+++ connect-app/src/components/MenuBar.js
@@ -1,29 +1,38 @@
 import React, { useState } from "react";
 import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 function MenuBar() {
-  const [activeItem, setActiveitem] = useState("");
+  const pathname = window.location.pathname;
+  const path = pathname === "/" ? "home" : pathname.substr(1);
+  const [activeItem, setActiveitem] = useState(path);
 
   const handleItemClick = (e, { name }) => setActiveitem(name);
 
   return (
-    <Menu pointing secondary>
+    <Menu pointing secondary size="massive" color="teal">
       <Menu.Item
         name="home"
         active={activeItem === "home"}
         onClick={handleItemClick}
+        as={Link}
+        to="/"
       />
 
       <Menu.Menu position="right">
-      <Menu.Item
-        name="login"
-        active={activeItem === "login"}
-        onClick={handleItemClick}
-      />
+        <Menu.Item
+          name="login"
+          active={activeItem === "login"}
+          onClick={handleItemClick}
+          as={Link}
+          to="/login"
+        />
         <Menu.Item
           name="register"
           active={activeItem === "register"}
           onClick={handleItemClick}
+          as={Link}
+          to="/register"
         />
       </Menu.Menu>
     </Menu>
